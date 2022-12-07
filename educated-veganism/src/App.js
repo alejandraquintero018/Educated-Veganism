a6ec1bc7be6e78f48e3798192b88da7e8996fcfc
alejandraquintero018 <alejandraquintero018@gmail.com>
2022-12-06 21:36:12 -0800

    initial commit and added a hero to the front

---
+++ educated-veganism/src/App.js
@@ -0,0 +1,82 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+/* This example requires Tailwind CSS v3.0+ */
+import { useState } from 'react'
+import { Dialog } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const navigation = [
+  { name: 'What', href: '#' },
+  { name: 'Why', href: '#' },
+  { name: 'How', href: '#' },
+  { name: 'How to Contribute', href: '#' },
+]
+
+export default function Example() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <div className="isolate bg-white">
+      <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
+      </div>
+      <div className="px-6 pt-6 lg:px-8">
+        <div>
+          <nav className="flex h-9 items-center justify-between" aria-label="Global">
+            <div className="flex lg:min-w-0 lg:flex-1" aria-label="Global">
+              {/* find a logo and insert later  */}
+            </div>
+            <div className="flex lg:hidden">
+              <button
+                type="button"
+                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                onClick={() => setMobileMenuOpen(true)}
+              >
+                <span className="sr-only">Open main menu</span>
+                <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div className="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-center lg:gap-x-12">
+              {navigation.map((item) => (
+                <a key={item.name} href={item.href} className="font-bold text-indigo-900 hover:text-gray-900">
+                  {item.name}
+                </a>
+              ))}
+            </div>
+            <div className="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-end">
+              <a
+                href="#"
+                className="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+              >
+                Log in
+              </a>
+            </div>
+          </nav>
+        
+        </div>
+      </div>
+      <main>
+        <div className="relative px-6 lg:px-8">
+          <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
+            <div>
+          
+              <div class="justify-center">
+                <h1 className=" font-mono text-9xl font-bold ">
+                  Educated Veganism
+                </h1>
+                <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
+                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt
+                  amet fugiat veniam occaecat fugiat aliqua.
+                </p>
+               
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+
---
+++ educated-veganism/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src"
+  ],
+  theme: {
+    fontFamily: {
+      'mono': ['monospace'],
+      'display': ['Oswald' ],
+      'body': ['"Open Sans"'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
