b0cc4dfd592f09f79ba324d1323df6b675c96c02
alejandraquintero018 <alejandraquintero018@gmail.com>
2022-12-29 00:29:21 -0800

    deployment

--- server/server.js
+++ server/server.js
@@ -19,11 +19,11 @@ app.use(express.json());
 
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '../client/build')));
+	app.use(express.static(path.join(__dirname, '../client')));
 }
 
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/public/index.html'));
 })
 
 // Create a new instance of an Apollo server with the GraphQL schema
